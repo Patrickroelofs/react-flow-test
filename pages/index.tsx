513f4b2ed0c4eab7cfb750ab1bcdf1ad20a40114
Patrick Roelofs <contact@patrickroelofs.com>
2023-03-05 21:24:29 +0100

    add drag and drop

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import { useCallback } from "react";
-import { addEdge, Node, Edge, ReactFlow, updateEdge, useEdgesState, useNodesState, Connection } from "reactflow";
+import { DragEvent, useCallback } from "react";
+import { addEdge, Node, Edge, ReactFlow, updateEdge, useEdgesState, useNodesState, Connection, ReactFlowProvider } from "reactflow";
 import 'reactflow/dist/style.css';
 
 const initialNodes: Node<any, string | undefined>[] = []
@@ -12,14 +12,39 @@ export default function Home() {
 
   return (
     <div className="wrapper">
-      <ReactFlow
-        nodes={nodes}
-        onNodesChange={onNodesChange}
-        edges={edges}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        fitView
-      />
+      <ReactFlowProvider>
+        <ReactFlow
+          nodes={nodes}
+          onNodesChange={onNodesChange}
+          edges={edges}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          fitView
+        />
+        <SideBar />
+      </ReactFlowProvider>
     </div>
   );
 }
+
+export const SideBar = () => {
+  const onDragStart = (event: DragEvent<HTMLDivElement>, nodeType: string) => {
+    event.dataTransfer.setData('application/reactflow', nodeType);
+    event.dataTransfer.effectAllowed = 'move';
+  };
+
+  return (
+    <aside>
+      <div className="description">You can drag these nodes to the pane on the right.</div>
+      <div className="dndnode input" onDragStart={(event) => onDragStart(event, 'input')} draggable>
+        Input Node
+      </div>
+      <div className="dndnode" onDragStart={(event) => onDragStart(event, 'default')} draggable>
+        Default Node
+      </div>
+      <div className="dndnode output" onDragStart={(event) => onDragStart(event, 'output')} draggable>
+        Output Node
+      </div>
+    </aside>
+  );
+};
